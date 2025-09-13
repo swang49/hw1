cd092e2965088daa68e42be9956daeb5e6576272
Sophie Wang <swang588@usc.edu>
2025-09-13 00:26:06 +0000

    Part 1 complete

--- split.cpp
+++ split.cpp
@@ -13,11 +13,41 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void addToEnd(Node*& head, Node* newNode);
 
-void split(Node*& in, Node*& odds, Node*& evens)
-{
+void split(Node*& in, Node*& odds, Node*& evens) {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+// Base case - in is an empty linked list
+  if(in == nullptr) {
+  return;
+  }
+
+// Implementation if in is NOT empty
+  Node* current = in;
+  Node* list = in->next;
+  current->next = nullptr;
+
+  split(list, odds, evens); // Recursively call function until only one element of in is left 
+
+  if(current->value % 2 == 0) { // If current value is even, add to evens
+    addToEnd(evens, current);
+  }
+  else {
+    addToEnd(odds, current); // If current value is odd, add to odds
+  }
+
+// Make sure to make in nullptr at the end
+  in = nullptr;
 }
 
 /* If you needed a helper function, write it here */
+void addToEnd(Node*& head, Node* newNode) { // helper function to make adding to evens and odds easier
+  if(head == nullptr) {
+    head = newNode;
+  }
+  else {
+    addToEnd(head->next, newNode);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -13,5 +13,88 @@ g++ split.cpp test_split.cpp -o test_split
 
 int main(int argc, char* argv[])
 {
+  // Case 1: null list
+  Node* emptyList = nullptr;
+  Node* odds = nullptr;
+  Node* evens = nullptr;
 
+  split(emptyList, odds, evens);
+
+  // Case 2: Randomized list {3, 4, 6, 7, 10}
+  Node* ranList = new Node{3, nullptr};
+  ranList->next = new Node{4, nullptr};
+  ranList->next->next = new Node{6, nullptr};
+  ranList->next->next->next = new Node{7, nullptr};
+  ranList->next->next->next->next = new Node{10, nullptr};
+
+  odds = nullptr;
+  evens = nullptr;
+
+  split(ranList, odds, evens);
+
+  Node* current = odds; // delete odds
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
+
+  current = evens; // delete evens
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
+
+  // Case 3: Only evens {2, 4, 6, 8, 10}
+  Node* evenList = new Node{2, nullptr};
+  evenList->next = new Node{4, nullptr};
+  evenList->next->next = new Node{6, nullptr};
+  evenList->next->next->next = new Node{8, nullptr};
+  evenList->next->next->next->next = new Node{10, nullptr};
+
+  odds = nullptr;
+  evens = nullptr;
+
+  split(evenList, odds, evens);
+
+  current = odds; // delete odds
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
+
+  current = evens; // delete evens
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
+
+  // Case 4: Only odds {1, 3, 5, 7, 9}
+  Node* oddList = new Node{1, nullptr};
+  oddList->next = new Node{3, nullptr};
+  oddList->next->next = new Node{5, nullptr};
+  oddList->next->next->next = new Node{7, nullptr};
+  oddList->next->next->next->next = new Node{9, nullptr};
+
+  odds = nullptr;
+  evens = nullptr;
+
+  split(oddList, odds, evens);
+
+  current = odds; // delete odds
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
+
+  current = evens; // delete evens
+  while(current != nullptr) {
+    Node* temp = current->next;
+    delete current;
+    current = temp;
+  }
 }
